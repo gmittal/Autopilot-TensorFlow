46b11b38def19b2d740d7b6763a6a03474314f2f
gmittal <gautam@mittal.net>
2017-07-28 22:54:36 -0700

    Add angle udacity conversion in run_dataset.py

--- run_dataset.py
+++ run_dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 import scipy.misc, math
 import model
@@ -9,6 +10,15 @@ def crop_udacity(img,cropx,cropy, y_off, x_off):
     startx = x//2-(cropx//2)+x_off
     starty = y//2-(cropy//2)+y_off
     return img[starty:starty+cropy,startx:startx+cropx]
+
+def angle_udacity(val):
+    norm = -4*np.arcsin(val)*180/np.pi
+    if np.isnan(norm):
+        diff = np.ceil(val) if val < 0 else np.floor(val)
+        start = -4*np.arcsin(val-diff)*180./np.pi
+        start += diff * -360.
+        return start
+    return norm
 
 sess = tf.InteractiveSession()
 saver = tf.train.Saver()
@@ -26,7 +36,8 @@ i = 0
 while(cv2.waitKey(10) != ord('q')):
     full_image = scipy.misc.imread("datasets/nvidia/" + str(i) + ".jpg", mode="RGB")
     image = scipy.misc.imresize(full_image[-150:], [66, 200]) / 255.0
-    degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180 / scipy.pi
+    degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] #* 180 / scipy.pi
+    degrees = angle_udacity(degrees)
     call("clear")
     print("Predicted steering angle: " + str(degrees) + " degrees")
     print("Ground truth steering angle: " + str(angles[i] * -25) + " degrees")
